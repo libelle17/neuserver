f97209a438ced76e7df9f799afe66397c93b9cc4
Gerald <gerald.schade@gmx.de>
2019-02-07 00:20:11 +0100

    Version 0.10001

--- los.sh
+++ los.sh
@@ -3,33 +3,50 @@ blau="\033[1;34m"
 reset="\033[0m"
 
 testuser() {
-	id -u "$1" >/dev/null 2>&1||{
-		echo -e "Benutzer $blau$1$reset:";
-	useradd -c"$2" -g praxis "$1";
-	passwd "$1";
-}
-pdbedit -L|grep "^$1:"||{
-	echo -e "Benutzer in Samba $blau$1$reset:";
-smbpasswd -a $1
- }
+		id -u "$1" >/dev/null 2>&1 &&obu=0||obu=1;
+		pdbedit -L|grep "^$1:" &&obs=0||obs=1;
+		passw="";
+		if test $obu -eq 1 -o $obs -eq 1; then {
+				echo -e "Bitte gewünschtes Passwort für Benutzer $blau$1$reset eingeben:";
+				read passw;
+		} fi;
+		if test $obu -eq 1; then {
+				echo -e "erstelle Linux-Benutzer $blau$1$reset";
+				useradd -p $(openssl passwd -1 $passw) -c"$2" -g praxis "$1";
+#               passwd "$1";
+#               userdel $1;
+		} fi;
+		if test $obs -eq 1; then {
+				echo -e "erstelle Samba-Benutzer $blau$1$reset";
+#               pdbedit -x -u $1;
+				echo -ne "$passw\n$passw\n"|smbpasswd -a -s $1
+#               smbclient -L //localhost/ -U $1
+		} fi;
 }
 
 test "$(id -u)" -eq "0"||{ echo "Wechsle zu root, bitte ggf. dessen Passwort eingeben:";su -c ./"$0";exit;};
 sed 's/:://;/\$/d;s/=/="/;s/$/"/;' vars>vars.sh
 . ./vars.sh
-hostnamectl
-echo -e $blau"gewünschter Servername, dann Enter:"$reset
-read SERVER
-hostnamectl set-hostname "$SERVER" 
-hostnamectl
+case $(hostname) in
+*-*) {
+		hostnamectl;
+		echo -e $blau"gewünschter Servername, dann Enter:"$reset;
+		read SERVER;
+		hostnamectl set-hostname "$SERVER";
+		export HOST="$SERVER";
+		hostnamectl; 
+};
+esac;
 grep -q "^praxis:" /etc/group||groupadd praxis
 $SPR samba 2>/dev/null||$IPR samba
-systemctl start smb 2>/dev/null|systemctl start smbd
-systemctl enable smb 2>/dev/null|systemctl enable smbd
-systemctl start nmb 2>/dev/null|systemctl start nmbd
-systemctl enable nmb 2>/dev/null|systemctl enable nmbd
+systemctl start smb 2>/dev/null||systemctl start smbd
+systemctl enable smb 2>/dev/null||systemctl enable smbd
+systemctl start nmb 2>/dev/null||systemctl start nmbd
+systemctl enable nmb 2>/dev/null||systemctl enable nmbd
 testuser schade "Gerald Schade"
 testuser sturm "T.Sturm"
 testuser simon "U.Simon"
 testuser vsftp "Benutzer zum Scannen von Brother MFC 8510DN über vsftp"
+testuser bittner "S.Bittner"
+
 
