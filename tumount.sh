d728818d59d2ebffee19c8d5f22d97eb58ccff90
Gerald Schade <geraldschade@gmx.de>
2019-02-23 01:15:16 +0100

    Version 0.47247

---
+++ tumount.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/awk -f
+/^[^#;]/ {
+# wenns das mount-Verzeichnis gibt
+if (!system("test -d "$2)) {
+	if(system("mountpoint -q "$2)) {
+		print "kein Mountpoint: "$2;
+		if(substr($1,1,5)=="LABEL") {
+			if(!system("blkid -L "substr($1,7))) {
+				system("mount -L "substr($1,7));
+				print "gibts: "substr($1,7);
+			} else {
+			  print "gibts nicht: "substr($1,7);
+  		}
+  	}
+  }
+}
+}
