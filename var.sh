58c2df01a9c223f9afb28d4d3eee349555486dc3
Gerald Schade <geraldschade@gmx.de>
2019-03-05 20:16:43 +0100

    Version 0.47247

--- var.sh
+++ var.sh
@@ -2,48 +2,90 @@
 blau="\e[1;34m";
 reset="\e[0m";
 credentials="libelle17:bach17raga"
-FB="fritz.box:49000"
-
-# > /dev/null 2>&1
-
-controlURL=/upnp/control/x_voip
-serviceType=urn:dslforum-org:service:X_VoIP:1
-Action=X_AVM-DE_DialNumber
-Pin=NewX_AVM-DE_PhoneNumber
-Rufnummer=616380;
+#credentials="schade:Stra_enbahn8"
+FB="http://fritz.box:49000"
+Ausgabe=neueurl.xml
+# curl $FB/tr64desc.xml
 
 controlURL=/igdupnp/control/WANCommonIFC1
 serviceType=urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1
 Action=GetCommonLinkProperties
-Pin=
+ParIn=
 
-controlURL=/upnp/control/x_contact
-serviceType=urn:dslforum-org:service:X_AVM-DE_OnTel:1
-Action=GetCallList
-Pin=
-
-controlURL=/upnp/control/x_storage
-serviceType=urn:dslforum-org:service:X_AVM-DE_Storage:1
+controlURL=x_storage
+serviceType=X_AVM-DE_Storage:1
 Action=GetInfo
-Pin=
+ParIn=
 
-controlURL=/upnp/control/x_filelinks
-serviceType=urn:dslforum-org:service:X_AVM-DE_Filelinks:1
+controlURL=x_contact
+serviceType=X_AVM-DE_OnTel:1
+Action=GetCallList
+ParIn=
+
+controlURL=deviceconfig
+serviceType=DeviceConfig:1
+Action=X_AVM-DE_CreateUrlSID
+ParIn=
+
+controlURL=x_filelinks
+serviceType=X_AVM-DE_Filelinks:1
 Action=GetFilelinkListPath
-Pin=
+ParIn=
 
-XML='<?xml version="1.0" encoding="utf-8" ?>
-<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
-  <s:Body>\n  <u:'$Action' xmlns:u="'$serviceType'"'
-[ -n "$Pin" ]&&XML=$XML'><'$Pin'>'${Rufnummer}'</'$Pin'></u:'$Action'>'||XML=$XML' />'
-XML=$XML'\n </s:Body>\n</s:Envelope>'
-	printf "$blau$XML$reset\n"
- 
-curl -4 -k --anyauth -u "${credentials}"                                   \
-  "http://${FB}$controlURL"                                     \
+controlURL=hosts
+serviceType=Hosts:1
+Action=X_AVM-DE_GetHostListPath
+ParIn=
+
+controlURL=x_voip
+serviceType=X_VoIP:1
+Action=X_AVM-DE_DialNumber
+ParIn=NewX_AVM-DE_PhoneNumber
+Inhalt="616380";
+
+case $controlURL in /*);;*) controlURL=/upnp/control/$controlURL;;esac;
+case $serviceType in urn:*);;*) serviceType=urn:dslforum-org:service:$serviceType;;esac;
+printf "controlURL: $blau$controlURL$reset\n"
+printf "serviceType: $blau$serviceType$reset\n"
+printf "Action: $blau$Action$reset\n"
+[ -n "$ParIn" ]&&printf "Parin: $blau$ParIn$reset\n";
+[ -n "$Inhalt" ]&&printf "Inhalt: $blau$Inhalt$reset\n";
+
+Soap="http://schemas.xmlsoap.org/soap";
+XML='<?xml version="1.0" encoding="utf-8"?>
+<s:Envelope s:encodingStyle="'$Soap'/encoding/" xmlns:s="'$Soap'/envelope/">
+  <s:Body><u:'$Action' xmlns:u="'$serviceType'"'
+[ -n "$ParIn" ]&&XML=$XML'><'$ParIn'>'${Inhalt}'</'$ParIn'></u:'$Action'>'||XML=$XML' />'
+XML=$XML'</s:Body>\n</s:Envelope>' # mit neue-Zeile-Zeichen nach <s:Body> ging anrufen nicht
+printf "XML derAbfrage: \n$blau$XML$reset\n"
+
+for ipv in 4 6;do
+curl -$ipv -k --anyauth -u "${credentials}"                                   \
+  "$FB$controlURL"                                     \
   -H 'Content-Type: text/xml; charset="utf-8"'                           \
   -H 'SoapAction: '$serviceType'#'$Action \
-  -d "$XML"
-echo ""
+	-d "$XML";printf "\n"
+erg=$(curl -$ipv -k --anyauth -u "${credentials}"                                   \
+  "$FB$controlURL"                                     \
+  -H 'Content-Type: text/xml; charset="utf-8"'                           \
+  -H 'SoapAction: '$serviceType'#'$Action \
+	-d "$XML";printf "\n")
+printf "Seifenaktion: "'SoapAction: '$serviceType'#'$Action 
+printf "\nRueckgabe: \n$blau$erg$reset\n";
+ret=1;
+case $erg in *.lua*)
+	nurl=$(echo "$erg"|awk '/\.lua/{print gensub(/^[^>]*>([^<]*)<.*/,"\\1","1")}')
+	case $nurl in *://*);;*)nurl=$FB$nurl;;esac
+	printf "Neue Url:\n$blau$nurl$reset\n";
+	curl -m 20 "$nurl" > $Ausgabe # "--connect-timeout 1" schuetzt leider nicht vor Fehler 606 bei ipv4 # oder |tee
+	ret=$?;
+	printf "\nAbfrageergebnis der neuen Url: $blau$ret$reset (Ergebnis in: $blau$Ausgabe$reset)\n"
+	[ "$ret" -eq 0 ]&&break;
+	;;
+	*)break;;
+esac;
+done;
+
+
 #netcat -4 fritz.box 1012
 
