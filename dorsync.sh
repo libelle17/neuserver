5e032349cf8625eedd06ad9a2310da94c97a39e8
Gerald Schade <gerald.schade@gmx.de>
2020-09-07 01:12:04 +0200

    +$D

---
+++ dorsync.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+dblau="\e[0;34;1;47m";
+reset="\e[0m";
+PROT="/var/log/rsync.log"
+kopier () {
+ echo Parameter in kopier: "$@"
+# tuez="rsync -avu $@"
+# tue=$(echo $tuez|sed 's/ -avu / -avu /g');
+ tue="rsync -avu $@"
+# echo -e $dblau$tuez$reset;
+# eval $tuez; erg=$?;
+##error in rsync protocol data stream (code 12)
+# if test $erg -eq 12; then 
+  echo -e $dblau$tue$reset;
+  eval $tue; 
+# fi;
+}
+echo `date +'%Y-%m-%d %H:%M:%S'` "Beginn rsync aus $0 \""$@"\"" >> "$PROT";
+#ionice -c 3 rsync -avu "$@"
+echo Parameter: "$@"
+kopier "$@";
+echo `date +'%Y-%m-%d %H:%M:%S'` "Ende rsync aus $0 \""$@"\"" >> "$PROT";
