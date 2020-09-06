6f8948523454d0c70a14fbdc3e365d89370addba
Gerald Schade <gerald.schade@gmx.de>
2020-09-06 23:16:38 +0200

    Version 0.47247

--- ziel.sh
+++ ziel.sh
@@ -9,27 +9,27 @@ for D in $(cat ziele);do
       Z=$(printf $D|sed 's/^[[]//;s/[]]$//;');;
     *)
       printf "$blau$D $Z/$D$reset\n";
-      [ -f $D ]&&AG=$(git log -1 --format="%at" -- $D)||AG=0;
-      [ -f $Z/$D ]&&AH=$(stat $Z/$D -c%Y)||AH=0;
-      echo AG: $AG;
-      echo AH: $AH;
-      [ 0$AH -eq 0 ]&&{
-        [ 0$AG -eq 0 ]||{
-         echo " "$D fehlt hier: cp -a $D $Z/;:;
+      [ -f $D ]&&AGit=$(git log -1 --format="%at" -- $D)||AGit=0;
+      [ -f $Z/$D ]&&AHier=$(stat $Z/$D -c%Y)||AHier=0;
+      echo AGit : $AGit;
+      echo AHier: $AHier;
+      [ 0$AHier -eq 0 ]&&{
+        [ 0$AGit -eq 0 ]&&{
+         echo " "$D fehlt hier und auf Git;:;
         }&&{
-         echo " "$D fehlt hier und auf Git;
+         echo " "$D fehlt hier: cp -a $D $Z/;
         };:;
       }||{
-        [ 0$AG -eq 0 ]&&{
+        [ 0$AGit -eq 0 ]&&{
          echo " "$D fehlt auf Git: cp -a $Z/$D .;:;
         }||{
           ls -l $Z/$D
           ls -l $D
           diff $Z/$D .;
           [ $? -eq 0 ]||printf "${rot}Dateien verschieden${reset}\n";
-          [ 0$AG -lt 0$AH ]&&echo " "$D auf Git aelter: cp -a $Z/$D .;
-          [ 0$AG -gt 0$AH ]&&echo " "$D hier aelter: cp -a $D $Z/;
-          [ 0$AG -eq 0$AH ]&&echo " "$D auf beiden gleich alt: lasse sie aus;
+          [ 0$AGit -lt 0$AHier ]&&echo " "$D auf Git aelter: cp -a $Z/$D .;
+          [ 0$AGit -gt 0$AHier ]&&echo " "$D hier aelter: cp -a $D $Z/;
+          [ 0$AGit -eq 0$AHier ]&&echo " "$D auf beiden gleich alt: lasse sie aus;
         }
       }
       ;;
