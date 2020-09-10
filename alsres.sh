0fdf20aee056ced8327b1cf1f09c85644268bab9
Gerald Schade <gerald.schade@gmx.de>
2020-09-10 23:45:04 +0200

    Version 0.47247

--- alsres.sh
+++ alsres.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
-RNAM=linux0
-SNAM=linux1
+NNAM=linux0
+ANAM=linux1
 # systemctl stop mysql
 systemctl disable poetd
 if false; then
-  if [[ $HOST == "$SNAM" ]]; then
+  if [[ $HOST == "$ANAM" ]]; then
 
 
       if test -d /var/lib/mysql_3; then
@@ -14,13 +14,13 @@ if false; then
        mv /var/lib/mysql_3 /var/lib/mysql
       fi
   else
-    if [[ $HOST == "$RNAM" ]]; then
+    if [[ $HOST == "$NNAM" ]]; then
     fi
   fi
 fi
-echo $RNAM.site > /etc/HOSTNAME
-echo $RNAM.site > /etc/hostname
-sed -i.bak 's/^192.168.178.46[[:space:]]\+$SNAM.site $SNAM/192.168.178.46  $RNAM.site $RNAM/g;s/^127.0.0.1[[:space:]]\+$SNAM.site $SNAM/127.0.0.1  $RNAM.site $RNAM/g' /etc/hosts 
-hostname $RNAM.site
-export hostname=$RNAM
+echo $NNAM.site > /etc/HOSTNAME
+echo $NNAM.site > /etc/hostname
+sed -i.bak 's/^192.168.178.46[[:space:]]\+$ANAM.site $ANAM/192.168.178.46  $NNAM.site $NNAM/g;s/^127.0.0.1[[:space:]]\+$ANAM.site $ANAM/127.0.0.1  $NNAM.site $NNAM/g' /etc/hosts 
+hostname $NNAM.site
+export hostname=$NNAM
 reboot
