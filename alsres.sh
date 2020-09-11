530b21a38feedc93856da4c1d09d167ddd18911e
Gerald Schade <gerald.schade@gmx.de>
2020-09-11 02:07:02 +0200

    Version 0.47247

--- alsres.sh
+++ alsres.sh
@@ -13,9 +13,9 @@ if false; then
        fi
        mv /var/lib/mysql_3 /var/lib/mysql
       fi
-  else
-    if [[ $HOST == "$NNAM" ]]; then
-    fi
+#  else
+#    if [[ $HOST == "$NNAM" ]]; then
+#    fi
   fi
 fi
 echo $NNAM.site > /etc/HOSTNAME
@@ -23,4 +23,4 @@ echo $NNAM.site > /etc/hostname
 sed -i.bak 's/^192.168.178.46[[:space:]]\+$ANAM.site $ANAM/192.168.178.46  $NNAM.site $NNAM/g;s/^127.0.0.1[[:space:]]\+$ANAM.site $ANAM/127.0.0.1  $NNAM.site $NNAM/g' /etc/hosts 
 hostname $NNAM.site
 export hostname=$NNAM
-reboot
+# reboot
