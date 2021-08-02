3b1499e6f3c23d4e1009039ff1d8430a41c9af8d
Gerald Schade <gerald.schade@gmx.de>
2021-08-02 02:31:16 +0200

    Version 0.47247

--- alsres.sh
+++ alsres.sh
@@ -11,7 +11,9 @@ systemctl stop mysql
 systemctl disable mysql
 systemctl stop poetd
 systemctl disable poetd
-if [ $HOST = "$ANAM" ]; then
+[ "$HOST" ]||HOST=$(hostname);
+echo HOST: $HOST, ANAM: $ANAM
+if [ $HOST = "$ANAM" -o \( -d /var/lib/mysql_3 -a -d /var/lib/mysql -a ! -d /var/lib/mysql_1 \) ]; then
     if test -d /var/lib/mysql_3; then
      if test -d /var/lib/mysql; then
        mv /var/lib/mysql /var/lib/mysql_1
