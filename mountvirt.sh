aacbf1a5cc070b1529a55e51f089048a2f26302e
Gerald Schade <gerald.schade@gmx.de>
2021-09-19 16:13:39 +0200

    Version 0.47247

--- mountvirt.sh
+++ mountvirt.sh
@@ -1,10 +1,69 @@
 #!/bin/bash
 ftb="/etc/fstab";
 cre="/home/schade/.wincredentials"
-[ -f "$cre" ]&&
-for gpc in virtwin virtwin0; do
- grep -q /$gpc/ $ftb||{
-  sed -i.bak -e "/^LABEL=/{i\//$gpc/Turbomed /mnt/$gpc/turbomed cifs nofail,vers=3.11,credentials=$cre 0 2" -e ":a;n;ba}" $ftb
- }
-done|| echo Datei $cre nicht gefunden!;
+verb=v;
+blau="\033[1;34m";
+dblau="\033[0;34;1;47m";
+rot="\033[1;31m";
+reset="\033[0m";
 
+# $1 = Befehl, $2 = Farbe, $3=obdirekt (ohne Result, bei Befehlen z.B. wie "... && Aktv=1" oder "sh ...")
+# in dem Befehl sollen zur Uebergabe erst die \ durch \\ ersetzt werden, dann die $ durch \$ und die " durch \", dann der Befehl von " eingerahmt
+ausf() {
+	[ "$verb" -o "$2" ]&&{ anzeige=$(echo "$2$1$reset\n"|sed 's/%/%%/'); printf "$anzeige";}; # escape für %, soll kein printf-specifier sein
+	if test "$3"; then 
+    eval "$1"; 
+  else 
+    resu=$(eval "$1"); 
+  fi;
+  ret=$?;
+  [ "$verb" ]&&{
+    printf "ret: $blau$ret$reset"
+    [ "$3" ]||printf ", resu: \"$blau$resu$reset\"";
+    printf "\n";
+  }
+} # ausf
+
+if ! test -f "$cre"; then 
+  echo Datei $cre nicht gefunden, breche ab!!;
+else
+  ergae=;
+  for iru in 1 2; do
+    for wirt in linux0 linux1 linux7; do
+     case $wirt in *0*) gpc=virtwin0; gast=Wind10;;
+                   *1*) gpc=virtwin;  gast=Win10;;
+                   *7*) gpc=virtwin7; gast=Wi10;;
+     esac;
+     case $(hostname) in $wirt*)tussh=;;*)tussh="ssh $wirt";;esac;
+     [ "$verb" ]&&echo iru: $iru, gpc: $gpc, wirt: $wirt, tussh: $tussh, gast: $gast
+     if [ "$iru" = 1 ]; then
+       grep -q /$gpc/ $ftb||{ 
+         [ "$ergae" ]&&ergae=$ergae\\n;
+         ergae=${ergae}"//$gpc/Turbomed /mnt/$gpc/turbomed cifs nofail,vers=3.11,credentials=$cre 0 2";
+       };
+     else
+       mp=/mnt/$gpc/turbomed;
+       [ -d "$mp" ]||mkdir -p "$mp";
+#      [ "$verb" ]&&echo mp: $mp, gpc: $gpc, tussh: $tussh, gast: $gast
+#       ping -c1 -W1 -q $gpc >/dev/null 2>&1||{ 
+       ausf "$tussh pgrep -f \" $gast \" >/dev/null"||{
+         [ "$verb" ]&&echo tussh: $tussh, gast: $gast; 
+         ausf "umount -a -t -cifs -l" $blau;
+         ausf "$tussh VBoxManage startvm $gast --type headless" $blau;
+         sleep 10;
+# das Folgende ist zumindest nicht durchgehend nötig
+#         ausf "ssh Administrator@$gpc netsh advfirewall firewall show rule name=Samba_aus_mountvird >NUL || netsh advfirewall firewall add rule name=\"Samba_aus_mountvirt\" dir=in action=allow protocol=tcp localport=445" $blau
+       };
+       mountpoint -q $mp||{ ausf "mount $mp" $blau; }
+     fi;
+    done;
+    if test "$iru" = 1 -a "$ergae"; then
+      if grep -q "^LABEL" $ftb; then
+#        [ "$verb" ]&&echo ergae: $ergae;
+        ausf "sed -i.bak -e \"/^LABEL=/{i $ergae\" -e \":a;n;ba}\" $ftb" $blau;
+      else
+        ausf "sed -i.bak \"$ a $ergae\" $ftb" $blau;
+      fi
+    fi;
+  done;
+fi;
--- nampruef.sh
+++ nampruef.sh
@@ -3,24 +3,42 @@
 # gast=virtwin0;
 # wirt=linux0;
 user=sturm;
-for wirt in linux0 linux1; do
-  case $wirt in linux0) 
-     gpc=virtwin0; 
-     gast=Wind10;; 
-    linux1) 
-     gpc=virtwin; 
-     gast=Win10;; 
-  esac;
-  echo wirt:  $wirt
-  echo gpc :   $gpc
-  echo gast: $gast
-  vip=$(ssh $wirt VBoxManage guestproperty get $gast /VirtualBox/GuestInfo/Net/2/V4/IP 2>/dev/null|cut -d' ' -f2);
-  echo vip:   $vip
-  if test "$vip"; then 
-    vhost=$(if nerg=$(nmblookup -A $vip); then echo $nerg|sed -n "s/.*"$vip"[[:space:]]*\([^[:space:]]*\).*/\L\1/p"; else ssh $user@$vip hostname|sed 's/\r$//'; fi); 
-    echo vhost: $vhost
-    if test "$vhost"/ != "$gpc"/; then 
-      ssh $user@$vip "WMIC computersystem where caption=\"$vhost\" rename \"$gpc\""; ssh $user@$vip shutdown -t 1 -r; 
-    fi; 
-  fi;
+[ "$1/" = "/v/" -o "$1/" = "-v/" -o "$1/" = "--verbose/" ]&&verb=v;
+blau="\033[1;34m";
+dblau="\033[0;34;1;47m";
+rot="\033[1;31m";
+reset="\033[0m";
+
+for wirt in linux0 linux1 linux7; do
+ case $wirt in *0*) gpc=virtwin0; gast=Wind10;;
+               *1*) gpc=virtwin;  gast=Win10;;
+               *7*) gpc=virtwin7; gast=Wi10;;
+ esac;
+ case $(hostname) in $wirt*)tussh=;;*)tussh="ssh $wirt";;esac;
+  printf "Wirt:  $blau$wirt$reset, gast: $blau$gast$reset, Soll-Gast-PC : $blau$gpc$reset";
+  [ "$verb" ]&& printf "\n";
+  # VBoxGuestAdditions im Gastsystem auf CD mounten und dort die exe aufrufen
+  # der ssh-public-key soll dort manuell an autorized_keys angehaengt werden
+  MaxNr=$($tussh VBoxManage guestproperty get $gast /VirtualBox/GuestInfo/Net/Count|cut -d' ' -f2);
+  [ "$verb" ]&&printf " MaxNr: $blau$MaxNr$reset\n";
+  for Nr in 0 1 2 3; do
+    vip=$($tussh VBoxManage guestproperty get $gast /VirtualBox/GuestInfo/Net/$Nr/V4/IP 2>/dev/null|cut -d' ' -f2);
+    [ "$verb" ]&&printf " vip:   $blau$vip$reset";
+    case $vip in 192.*)
+      [ "$verb" ]&&printf " (vielleicht passend)";
+      vhost=$(ping -c1 -W1 -q "$vip" >/dev/null&&if nerg=$(nmblookup -A $vip); then 
+                echo $nerg|sed -n "s/.*"$vip"[[:space:]]*\([^[:space:]]*\).*/\L\1/p"; else ssh $user@$vip hostname|sed 's/\r$//'; 
+              fi); 
+      [ -z "$verb" -a "$vhost" ]&&printf ", vip: $blau$vip$reset";
+      [ "$verb" -o "$vhost" ]&&printf ", Adapter Nr: $blau$Nr$reset, vhost aus nmblookup: $blau$vhost$reset\n"
+      if test "$vhost" -a "$vhost"/ != "$gpc"/; then 
+        printf "$rot benenne $vhost in $gpc um!$reset\n";
+        ssh $user@$vip "WMIC computersystem where caption=\"$vhost\" rename \"$gpc\""; ssh $user@$vip shutdown -t 1 -r; 
+      fi; 
+      [ "$vhost" ]&&break;;
+     *)
+       [ "$verb" ]&& printf "\n";
+    esac;
+    [ "$Nr"/ = "$MaxNr"/ ]&&break;
+  done;
 done;
