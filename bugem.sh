9edcc18fc1c824dad885a8bccc926afe0287cd28
Gerald Schade <gerald.schade@gmx.de>
2021-10-02 00:46:00 +0200

    Version 0.47247

--- bugem.sh
+++ bugem.sh
@@ -126,7 +126,9 @@ kopiermt() { # mit test
   QVofs=$(echo ${1#/}|sed 's/\([^\\]\) /\1\\ /g'); # Quellverzeichnis ohne führenden slash, mit "\ " statt " "
   QVos=${QVofs%/};
   case $QVofs in */)obsub=;;*)obsub=1;;esac;
-  [ "$obsub" ]&&{ eval '$qssh "[ -f \"/$QVos\" ]"&&obdat=1||obdat=';};
+  echo obsub: $obsub    !!!!!!!!!!!!
+  [ "$obsub" ]&&{ eval "$qssh '[ -f \"/$QVos\" ]'&&obdat=1||obdat=";};
+  echo obdat: $obdat    !!!!!!!!!!!!
   [ "$obsub" ]&&{ $qssh "[ -f \"/$QVos\" ]"&&obdat=1||obdat=;}; # das geht nicht mit zsh
   if [ -z "$2" -o "$2" = "..." ]; then ZVofs=${QVofs%/*}/; [ "$ZVofs" = "$QVofs/" ]&&ZVofs=""; else # letzteres für QVofs ohne /
   ZVofs=$(echo ${2#/}|sed 's/\([^\\]\) /\1\\ /g'); fi; # Zielverzeichnis ohne führenden slash, mit "\ " statt " "
