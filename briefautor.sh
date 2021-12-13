915bd6eed850779fc3a1563a006b1e83ce4dd512
Gerald Schade <gerald.schade@gmx.de>
2021-12-14 00:57:30 +0100

    Version 0.47247

--- briefautor.sh
+++ briefautor.sh
@@ -26,26 +26,26 @@ commandline() {
 commandline "$@"; # alle Befehlszeilenparameter übergeben
 for jahr in $(seq 2004 1 2050); do
   [ "$verb" ]&&echo $jahr
-  for D in $(mysql --defaults-file=~/.mysqlpwd -B -e"SELECT  concat(ID,'<$>',REPLACE(REPLACE(REPLACE(REPLACE(pfad,'\$','/DATA'),'\\\\TurboMed\\\\','/turbomed/'),'\\\\','/'),' ','°³²°')) '' FROM quelle.briefe where isnull(autor) and (year(quelldatum)<=$jahr or ($jahr=2050)) and pfad like '%\.pdf'"); do
+  for D in $(mysql --defaults-file=~/.mysqlpwd -B -e"SELECT  concat(ID,'<$>',REPLACE(REPLACE(REPLACE(REPLACE(pfad,'\$','/DATA'),'\\\\TurboMed\\\\','/turbomed/'),'\\\\','/'),' ','°³²°'),'<$>',date(quelldatum),'<$>',replace(name,' ','°³²°')) '' FROM quelle.briefe where isnull(autor) and (year(quelldatum)<=$jahr or ($jahr=2050)) and pfad like '%\.pdf'"); do
 # for D in "128072<$>/DATA/turbomed/Dokumente/Sonstiges/202106/Strasser19370517-54583-8FFF40CC-DB1F-48a0-BBFD-F323A6AC5DC9.pdf"; do
-  arr=(${D//<$>/ });
-  arr[1]=${arr[1]//°³²°/ };
-  if stat "${arr[1]}" >/dev/null 2>&1; then
-    autor=-;
-    for indx in $(seq 0 1 ${#abk}); do
-      if [ $indx = ${#sus} ]; then break; fi;
-#      [ "$verb" ]&&echo $indx: ${sus[$indx]} # zu ausführlich
-      if pdftotext "${arr[1]}" - 2>/dev/null|grep "${sus[$indx]}" >/dev/null; then
-        autor=${abk[$indx]};
-        [ "$verb" ]&&echo ${arr[0]} ${arr[1]}: ${abk[$indx]} ja;
-        break;
-      fi;
-    done;
-    [ "$verb" -a "$autor" = "-" ]&&echo ${arr[0]} ${arr[1]}: nein;
-  else
-   autor='?';
-   [ "$verb" ]&&printf "$rot${arr[0]} $blau${arr[1]}$reset: ${rot}nicht gefunden$reset\n";
-  fi;
-  mysql --defaults-file=~/.mysqlpwd -B -e"UPDATE quelle.briefe SET autor=\"$autor\" WHERE id=${arr[0]}";
-done;
-done;
+    arr=(${D//<$>/ });
+    arr[1]=${arr[1]//°³²°/ };
+    arr[3]=${arr[3]//°³²°/ };
+    if stat "${arr[1]}" >/dev/null 2>&1; then
+      autor=-;
+      for indx in $(seq 0 1 ${#abk}); do
+        if [ $indx = ${#sus} ]; then break; fi;
+  #      [ "$verb" ]&&echo $indx: ${sus[$indx]} # zu ausführlich
+        if pdftotext "${arr[1]}" - 2>/dev/null|grep "${sus[$indx]}" >/dev/null; then
+          autor=${abk[$indx]};
+          break;
+        fi;
+      done;
+      [ "$verb"  ]&&printf "$jahr: $blau${arr[0]}$reset ${arr[3]} $blau${arr[2]}$reset: $dblau$autor$reset\n";
+    else
+     autor='?';
+     [ "$verb" ]&&printf "$jahr: $rot${arr[0]} $blau${arr[3]} ${arr[2]}$reset: ${rot}nicht gefunden$reset\n";
+    fi;
+    mysql --defaults-file=~/.mysqlpwd -B -e"UPDATE quelle.briefe SET autor=\"$autor\" WHERE id=${arr[0]}";
+  done; # for D in
+done; # for jahr in 
