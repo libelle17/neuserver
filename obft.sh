5b33979547de0c1c19a4a3cc38b56e16cbb0e357
Gerald Schade <gerald.schade@gmx.de>
2021-12-30 23:57:00 +0100

    Version 0.47247

--- obft.sh
+++ obft.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 today=$(date --date="0 days ago " +"%-d-%-m-%Y");
 if echo "$today"|grep -q ^24-12; then exit 0; fi;
-if echo "$today"|grep -q ^30-12; then exit 0; fi;
 if echo "$today"|grep -q ^31-12; then exit 0; fi;
 json_return=$(curl -s http://www.kayaposoft.com/enrico/json/v1.0/?action=isPublicHoliday\&date=$today\&country=ger )
 if echo "$json_return"|grep -q true; then exit 0; fi;
