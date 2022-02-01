a82f570386f372b519ee22baf16c49673d8b9902
Gerald Schade <gerald.schade@gmx.de>
2022-02-02 00:07:03 +0100

    Version 0.47247

--- vdurch.sh
+++ vdurch.sh
@@ -3,8 +3,6 @@ MUPR=$(readlink -f $0); # Mutterprogramm
 . ${MUPR%/*}/bul1.sh # LINEINS=linux1, buhost festlegen
 . ${MUPR%/*}/bugem.sh # commandline-Parameter, $ZL aus commandline, $qssh, $zssh festlegen
 wirt=$buhost;
-echo wirt: $wirt
-exit
 . ${MUPR%/*}/virtnamen.sh # legt aus $wirt fest: $gpc, $gast, $tush
 ot=/opt/turbomed;
 ausf "$tush 'mv /$ot/lauf /$ot/lau  2>/dev/null||touch /$ot/lau'&&sleep 80s";
