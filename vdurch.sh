33e5ff8d075d9bb573f6844233ed73afc339480c
Gerald Schade <gerald.schade@gmx.de>
2022-02-01 23:05:24 +0100

    Version 0.47247

---
+++ vdurch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+MUPR=$(readlink -f $0); # Mutterprogramm
+. ${MUPR%/*}/bugem.sh # commandline-Parameter, $ZL aus commandline, $qssh, $zssh festlegen
+wirt=$buhost;
+. ${MUPR%/*}/virtnamen.sh # legt aus $wirt fest: $gpc, $gast, $tush
+ausf "$tush 'mv /$ot/lauf /$ot/lau  2>/dev/null||touch /$ot/lau'&&sleep 80s";
+VBoxManage controlvm Win10 poweroff; VBoxManage startvm Win10 --type headless;
+mv /$ot/lau /$ot/lauf 2>/dev/null||touch /$ot/lauf; # zurückbenennen, damit Turbomed wieder starten kann
