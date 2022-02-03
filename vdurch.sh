a30977bed7b37f042e96e21ea94df60c6dfb1deb
Gerald Schade <gerald.schade@gmx.de>
2022-02-03 14:15:14 +0100

    Version 0.47247

--- vdurch.sh
+++ vdurch.sh
@@ -4,12 +4,11 @@ MUPR=$(readlink -f $0); # Mutterprogramm
 . ${MUPR%/*}/bugem.sh # commandline-Parameter, $ZL aus commandline, $qssh, $zssh festlegen
 wirt=$buhost;
 . ${MUPR%/*}/virtnamen.sh # legt aus $wirt fest: $gpc, $gast, $tush
-echo Stelle 1
 ot=/opt/turbomed;
-echo Stelle 2
+echo Benenne /$ot/lauf zu /$ot/lau um und warte 80 Sekunden
 ausf "$tush 'mv /$ot/lauf /$ot/lau  2>/dev/null||touch /$ot/lau'&&sleep 80s";
-echo Stelle 3
+echo  Starte den virtuellen Server Win10 durch
 VBoxManage controlvm Win10 poweroff; VBoxManage startvm Win10 --type headless;
-echo Stelle 4
+echo Benenne /$ot/lau wieder zu /$ot/lauf um
 mv /$ot/lau /$ot/lauf 2>/dev/null||touch /$ot/lauf; # zurückbenennen, damit Turbomed wieder starten kann
-echo Stelle 5
+echo Fertig!
