507d25a53f75fbdeaa3288ff75a330e11d365551
Gerald Schade <gerald.schade@gmx.de>
2022-02-23 14:55:49 +0100

    Version 0.47247

--- srv/www/htdocs/php/i0S.php
+++ srv/www/htdocs/php/i0S.php
@@ -11,10 +11,6 @@
     $_SESSION['OAU']=!$_SESSION['OAU'];
  } else  IF(isset($_POST['HbA1c'])) {
     $_SESSION['HbA1c']=!$_SESSION['HbA1c'];
- } else  IF(isset($_POST['Uzu'])) {
-    $_SESSION['Uzu']=!$_SESSION['Uzu'];
- } else  IF(isset($_POST['Hypo'])) {
-    $_SESSION['Hypo']=!$_SESSION['Hypo'];
  } else  IF(isset($_POST['Hyper'])) {
     $_SESSION['Hyper']=!$_SESSION['Hyper'];
  } else  IF(isset($_POST['NeuSt'])) {
--- srv/www/htdocs/php/i3S.php
+++ srv/www/htdocs/php/i3S.php
@@ -6,8 +6,6 @@
  $_SESSION['obRR']=0;
  $_SESSION['obOAU']=0;
  $_SESSION['obHbA1c']=0;
- $_SESSION['obUzu']=0;
- $_SESSION['obHypo']=0;
  $_SESSION['obHyper']=0;
  $_SESSION['obNeuSt']=0;
  $_SESSION['obFuß']=0;
