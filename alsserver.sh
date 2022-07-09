11c448fa2e54380343c9a0d99d7cb628df528ed5
Gerald Schade <gerald.schade@gmx.de>
2022-07-09 14:38:37 +0200

    Version 0.47247

--- alsserver.sh
+++ alsserver.sh
@@ -18,4 +18,6 @@ IP=$(ip route get 1 | awk '{print $(NF-2);exit;}');
 sed -i.bak 's/^'$IP'[[:space:]]\+'$HOST'.site '$HOST'/'$IP' linux1.site linux1/g;s/^127.0.0.1[[:space:]]\+'$HOST'.site '$HOST'/127.0.0.1  linux1.site linux1/g' /etc/hosts
 hostname linux1.site
 export hostname=linux1
+D=obslaeuft; mkdir -p /$D; touch /$D/laeuft; chown -R sturm:praxis /$D
 # reboot
+
