6f9824f50b5f080b1875d14a0d66afb972f71714
Gerald Schade <gerald.schade@gmx.de>
2022-07-23 12:08:05 +0200

    Version 0.47247

---
+++ dbeinpacken.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+dbs=$(mysql --defaults-extra-file=~/.mysqlpwd -BNe 'show databases' | grep -vE '^mysql$|^(performance|information)_schema$')
+echo $dbs>dbverzeichnis
+mysqldump --defaults-extra-file=~/.mysqlpwd --default-character-set=utf8mb4 --complete-insert --compress --disable-keys --routines --events --triggers --databases $dbs > ~/dbeingepackt.sql
+#    echo "$dbs" | while read -r db; do
+#        mysqladmin drop "$db"
+#    done && \
+#    mysql -e 'SET GLOBAL innodb_fast_shutdown = 0' && \
+#    /etc/init.d/mysql stop && \
+#    rm -f /var/lib/mysql/ib{data1,_logfile*} && \
+#    /etc/init.d/mysql start && \
+#    mysql < alldatabases.sql
