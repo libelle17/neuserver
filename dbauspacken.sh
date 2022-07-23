77c47d4f7c0c2cb788ba911ba11c192911898c44
Gerald Schade <gerald.schade@gmx.de>
2022-07-23 13:31:24 +0200

    Version 0.47247

--- dbauspacken.sh
+++ dbauspacken.sh
@@ -2,7 +2,6 @@
 HOST=$(hostname);
 # Datenverzeichnis von mysql
 VLM=$(sed -n 's/^[[:space:]]*datadir[[:space:]]*=[[:space:]]*\(.*\)/\1/p' /etc/my.cnf); VLM=${VLM:-/var/lib/mysql};
-echo VLM: $VLM;
 # nicht auf linux1, um nichts falsches zu löschen
 if [ ${HOST%%.*}/ != linux1/ ]; then
   # wenn dbeingepackt frisch erstellt und kopiert wurde
@@ -13,17 +12,17 @@ if [ ${HOST%%.*}/ != linux1/ ]; then
      for db in $(cat ~/dbverzeichnis);do 
        echo " "$db;
        # die alten Datenbanken löschen 
-       mysqladmin --defaults-extra-file=~./mysqlpwd -f drop $db; 
+       mysqladmin --defaults-extra-file=~/.mysqlpwd -f drop $db; 
        # und ggf. Reste löschen
        rm -rf $VLM/$db;
      done;
-     mysql --defaults-extra-file=~./mysqlpwd -e 'SET GLOBAL innodb_fast_shutdown = 0';
+     mysql --defaults-extra-file=~/.mysqlpwd -e 'SET GLOBAL innodb_fast_shutdown = 0';
      systemctl stop mariadb;
      # Datenbank gleich packen
      rm -f $VLM/ib{data1,_logfile0}
      systemctl start mariadb;
      # und Daten übertragen
-     mysql --defaults-extra-file=~./mysqlpwd </root/dbeingepackt.sql;
+     mysql --defaults-extra-file=~/.mysqlpwd </root/dbeingepackt.sql;
    done;
   done;
 fi;
