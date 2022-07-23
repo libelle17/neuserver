2a01a7e408c562a8661ef1373797d45bfe668e58
Gerald Schade <gerald.schade@gmx.de>
2022-07-23 11:50:06 +0200

    Version 0.47247

--- zeigtabellen.sh
+++ zeigtabellen.sh
@@ -2,7 +2,7 @@
 # Anzeige des Mysql-Datenbankinhalts zum Überprüfen der Integrität nach Kopie
 mysql --defaults-extra-file=~/.mysqlpwd -e"SELECT CONCAT(TABLE_SCHEMA,'.\`',TABLE_NAME,'\`') FROM information_schema.tables WHERE TABLE_TYPE='BASE TABLE'"|tail -n +2|while read zeile; do
    mysql --defaults-extra-file=~/.mysqlpwd quelle -e"select count(0) from $zeile"|tail -n +2|while read erg; do 
-    printf " %70s %10s\n" "$zeile" "$erg";
+    printf " %75s %10s\n" "$zeile" "$erg";
     break; 
    done;
 done;
