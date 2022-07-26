473eb79ad0f9f9faa97ceffd1cd22364809e7029
Gerald Schade <gerald.schade@gmx.de>
2022-07-26 23:35:22 +0200

    Version 0.47247

--- alsres.sh
+++ alsres.sh
@@ -7,20 +7,21 @@ fi;
 NNAM=$1;
 IP=$2;
 ANAM=linux1
-systemctl stop mysql
-systemctl disable mysql
+# mysql-Aktion am 26.7.22 auskommentiert
+# systemctl stop mysql
+# systemctl disable mysql
 systemctl stop poetd
 systemctl disable poetd
 [ "$HOST" ]||HOST=$(hostname);
 echo HOST: $HOST, ANAM: $ANAM
-if [ $HOST = "$ANAM" -o \( -d /var/lib/mysql_3 -a -d /var/lib/mysql -a ! -d /var/lib/mysql_1 \) ]; then
-    if test -d /var/lib/mysql_3; then
-     if test -d /var/lib/mysql; then
-       mv /var/lib/mysql /var/lib/mysql_1
-     fi
-     mv /var/lib/mysql_3 /var/lib/mysql
-    fi
-fi
+# if [ $HOST = "$ANAM" -o \( -d /var/lib/mysql_3 -a -d /var/lib/mysql -a ! -d /var/lib/mysql_1 \) ]; then
+#     if test -d /var/lib/mysql_3; then
+#      if test -d /var/lib/mysql; then
+#        mv /var/lib/mysql /var/lib/mysql_1
+#      fi
+#      mv /var/lib/mysql_3 /var/lib/mysql
+#     fi
+# fi
 echo $NNAM.site > /etc/HOSTNAME
 echo $NNAM.site > /etc/hostname
 sed -i.bak 's/^'$IP'[[:space:]]\+$ANAM.site $ANAM/'$IP' $NNAM.site $NNAM/g;s/^127.0.0.1[[:space:]]\+$ANAM.site $ANAM/127.0.0.1  $NNAM.site $NNAM/g' /etc/hosts 
--- alsserver.sh
+++ alsserver.sh
@@ -1,17 +1,19 @@
 #!/bin/zsh
 # soll vor Verwendung des aktuellen Linux-PC als Server aufgerufen werden
-systemctl stop mysql
-systemctl stop mysql
+
+# mysql-Aktion am 26.7.22 auskommentiert
+# systemctl stop mysql
+# systemctl stop mysql
 systemctl enable poetd
 [ "$HOST" ]||HOST=$(hostname);
-if [ "$HOST" != "linux1" ]; then
-  if test -d /var/lib/mysql_1; then
-   if test -d /var/lib/mysql; then
-     mv /var/lib/mysql /var/lib/mysql_3
-   fi
-   mv /var/lib/mysql_1 /var/lib/mysql
-  fi
-fi
+# if [ "$HOST" != "linux1" ]; then
+#   if test -d /var/lib/mysql_1; then
+#    if test -d /var/lib/mysql; then
+#      mv /var/lib/mysql /var/lib/mysql_3
+#    fi
+#    mv /var/lib/mysql_1 /var/lib/mysql
+#   fi
+# fi
 
 echo linux1.site > /etc/HOSTNAME
 echo linux1.site > /etc/hostname
@@ -25,5 +27,6 @@ systemctl enable smb;
 systemctl start smb;
 systemctl enable nmb;
 systemctl start nmb;
+autofax -cm 2;
 # reboot
 
