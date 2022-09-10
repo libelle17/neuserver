4791a91f87e7c11761f1e2a3b3b8bcfb30d138b9
Gerald Schade <gerald.schade@gmx.de>
2022-09-10 14:46:51 +0200

    Version 0.47247

--- virtnamen.sh
+++ virtnamen.sh
@@ -1,9 +1,8 @@
 # for wirt in "$auswahl"; do
- case $wirt in *0*) gpc=virtwin0; gast=Win10;;
-               *1*) gpc=virtwin;  gast=Win10;;
+ case $wirt in *0*) gpc=/virtwin0; gast=Win10;;
+               *1*) gpc=/virtwin;  gast=Win10;;
                *3*) gpc=;         gast=;;
-               *7*) gpc=virtwin7; gast=Win10;;
-               *8*) gpc=virtwin8; gast=Win10;;
+               *7*) gpc=/virtwin7; gast=Win10;;
+               *8*) gpc=/virtwin8; gast=Win10;;
  esac;
- gpc=/$gpc; # 10.9.22
  case $wirt in $LINEINS)tush="sh -c ";;*)tush="ssh $wirt ";;esac
