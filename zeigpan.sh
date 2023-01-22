78e20a61899bce3202b8defd3df01721ab842225
Gerald Schade <gerald.schade@gmx.de>
2023-01-23 00:39:28 +0100

    Version 0.47247

--- zeigpan.sh
+++ zeigpan.sh
@@ -49,6 +49,7 @@ pr=PraxisDB;
 echo "Virtuelle Windows-Server:"
 MUPR=$(readlink -f $0); # Mutterprogramm
 . ${MUPR%/*}/bul1.sh # LINEINS=linux1, buhost festlegen
+. ${MUPR%/*}/bugem.sh # commandline-Parameter, $ZL aus commandline, $qssh, $zssh festlegen
 ziele="0 3 7 8"; # Vorgaben für Ziel-Servernummern: linux0, linux3 usw.
 for nr in 1 $ziele; do
  wirt=linux$nr;
