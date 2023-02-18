22666534c84f8e0b30acc7ca118ad48030fd8506
Gerald Schade <gerald.schade@gmx.de>
2023-02-18 11:24:22 +0100

    Version 0.47247

--- fzst.sh
+++ fzst.sh
@@ -3,5 +3,5 @@ if test $# != 2; then
   echo $0: Listet den Füllungszustand aller gefüllten Tabellen einer Datenbank eines Servers auf
   echo Gebrauch: "$0 <datenbank> <rechner>"
 else
-DB=$1;SV=$2;ssh $SV "mysql --defaults-extra-file=~/.mysqlpwd $DB -e \"SELECT CONCAT(RPAD(table_name,30,'.'),TABLE_ROWS) \\\`$SV $DB:\\\` FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'quelle' AND NOT ISNULL(table_rows) AND table_rows<>0 ORDER BY table_name\""
+DB=$1;SV=$2;ssh $SV "mysql --defaults-extra-file=~/.mysqlpwd $DB -e \"SELECT CONCAT(RPAD(table_name,40,'.'),TABLE_ROWS) \\\`$SV $DB:\\\` FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '$DB' AND NOT ISNULL(table_rows) AND table_rows<>0 ORDER BY table_name\""
 fi
