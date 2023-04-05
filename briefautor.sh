c55ba5525091d058dc154c36bc1e0c8e9f675be9
Gerald Schade <gerald.schade@gmx.de>
2023-04-05 23:47:43 +0200

    Version 0.47247

--- briefautor.sh
+++ briefautor.sh
@@ -7,12 +7,13 @@ rot="\033[1;31m";
 reset="\033[0m";
 # Befehlszeilenparameter auswerten
 commandline() {
+  verb=0;
 	while [ $# -gt 0 ]; do
    case "$1" in 
      -*|/*)
       para=${1#[-/]};
       case $para in
-        v|-verbose) verb=1;;
+        v|-verbose) if [ $verb/ = 0/ ]; then verb=1; else verb=2; fi;;
       esac;;
    esac;
    shift;
@@ -26,7 +27,7 @@ commandline() {
 commandline "$@"; # alle Befehlszeilenparameter übergeben
 for jahr in $(seq 2004 1 2050); do
   [ "$verb" ]&&echo $jahr
-  for D in $(mysql --defaults-file=~/.mysqlpwd -B -e"SELECT  concat(ID,'<$>',REPLACE(REPLACE(REPLACE(REPLACE(pfad,'\$','/DATA'),'\\\\TurboMed\\\\','/turbomed/'),'\\\\','/'),' ','°³²°'),'<$>',date(quelldatum),'<$>',replace(name,' ','°³²°')) '' FROM quelle.briefe where isnull(autor) and (year(quelldatum)<=$jahr or ($jahr=2050)) and pfad like '%\.pdf'"); do
+  for D in $(mysql --defaults-file=~/.mysqlpwd -B -e"SELECT  CONCAT(ID,'<$>',REPLACE(REPLACE(REPLACE(REPLACE(pfad,'\$','/DATA'),'\\\\TurboMed\\\\','/turbomed/'),'\\\\','/'),' ','°³²°'),'<$>',DATE(quelldatum),'<$>',REPLACE(name,' ','°³²°')) '' FROM quelle.briefe WHERE (autor='' OR ISNULL(autor)) AND (YEAR(quelldatum)<=$jahr or ($jahr=2050)) AND pfad LIKE '%\.pdf'"); do
 # for D in "128072<$>/DATA/turbomed/Dokumente/Sonstiges/202106/Strasser19370517-54583-8FFF40CC-DB1F-48a0-BBFD-F323A6AC5DC9.pdf"; do
     arr=(${D//<$>/ });
     arr[1]=${arr[1]//°³²°/ };
@@ -35,13 +36,13 @@ for jahr in $(seq 2004 1 2050); do
       autor=-;
       for indx in $(seq 0 1 ${#abk}); do
         if [ $indx = ${#sus} ]; then break; fi;
-  #      [ "$verb" ]&&echo $indx: ${sus[$indx]} # zu ausführlich
+        [ "$verb"/ = 2/ ]&&echo $indx: ${sus[$indx]} # zu ausführlich
         if pdftotext "${arr[1]}" - 2>/dev/null|grep "${sus[$indx]}" >/dev/null; then
           autor=${abk[$indx]};
           break;
         fi;
       done;
-      [ "$verb"  ]&&printf "$jahr: $blau${arr[0]}$reset ${arr[3]} $blau${arr[2]}$reset: $dblau$autor$reset\n";
+      [ "$verb" ]&&printf "$jahr: $blau${arr[0]}$reset ${arr[3]} $blau${arr[2]}$reset: $dblau$autor$reset\n";
     else
      autor='?';
      [ "$verb" ]&&printf "$jahr: $rot${arr[0]} $blau${arr[3]} ${arr[2]}$reset: ${rot}nicht gefunden$reset\n";
