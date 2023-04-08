a57ce6af6a68b7fadbdb92e1e1c287d5966b1816
Gerald Schade <gerald.schade@gmx.de>
2023-04-08 10:38:47 +0200

    Version 0.47247

--- briefautor.sh
+++ briefautor.sh
@@ -39,7 +39,8 @@ for jahr in $(seq 2004 1 $(date +%Y)); do
 "(name LIKE 'CGM BMP gedruckt%') OR "\
 "(name RLIKE '^[^,]*(Uew|Ü[Ww]) *.{0,12}$') OR "\
 "(name RLIKE '_2[0-9]\{3\}[-_][0-9]\{2\}[-_][0-9]\{2\}.*\.pdf$') OR "\
-"true)"); do
+"(name RLIKE '20[0-9]\{12\}[^0-9]') OR "\
+"false)"); do
 # for D in "128072<$>/DATA/turbomed/Dokumente/Sonstiges/202106/Strasser19370517-54583-8FFF40CC-DB1F-48a0-BBFD-F323A6AC5DC9.pdf"; do
     arr=(${D//<$>/ }); # 0: id, 1: Pfad, 2: quelldatum, 3: Name
     arr[1]=${arr[1]//°³²°/ };
@@ -65,6 +66,10 @@ for jahr in $(seq 2004 1 $(date +%Y)); do
           erga=$(echo ${arr[3]}|sed -n "s/^.*_\(2[0-9]\{3\}[-_][0-9]\{2\}[-_][0-9]\{2\}\).*$/\1/;s/_/-/g;1p");
           [ $verb -ge 2 ]&&printf "%4s(3): ${arr[2]} $blau%60s$reset ${arr[0]} => $lila${erga}$reset\n" $nr "${arr[3]}";
           mysql --defaults-file=~/.mysqlpwd -B -e"UPDATE quelle.briefe SET autor=\"-\",quelldatum=STR_TO_DATE(\""${erga}\"",\"%Y-%c-%d\") WHERE id=${arr[0]}";
+        elif echo ${arr[3]}|egrep -q "20[0-9]{12}[^0-9]"; then
+          erga=$(echo ${arr[3]}|sed -n "s/^.*\(20[0-9]\{12\}\)[^0-9].*$/\1/;1p");
+          [ $verb -ge 2 ]&&printf "%4s(4): ${arr[2]} $blau%60s$reset ${arr[0]} => $lila${erga}$reset\n" $nr "${arr[3]}";
+          mysql --defaults-file=~/.mysqlpwd -B -e"UPDATE quelle.briefe SET autor=\"-\",quelldatum=STR_TO_DATE(\""${erga}\"",\"%Y%c%d%H%i%S\") WHERE id=${arr[0]}";
         else
           [ $verb -ge 2 ]&&printf "$nr: ${arr[0]} ${arr[2]} $lila${arr[3]}$reset ${arr[1]}\n";
         fi;
