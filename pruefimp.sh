c72b47256bc7cd46c3652305996c68644141aa9d
Gerald Schade <gerald.schade@gmx.de>
2023-07-02 23:15:08 +0200

    Version 0.47247

--- pruefimp.sh
+++ pruefimp.sh
@@ -15,14 +15,14 @@ nr=0
 fnr=0
 verb=
 printf "suche ..."
-find $D -mindepth 1 -maxdepth 3 -newermt 20091231 -type f -not -iregex '.*\(azubi\|anforderung\|bei geräten\|dmp-daten\|diabetestag\|dokumentation.*htm\|fortbildung\|hausärzteverb\|schweigepflichts+entbindung\|vhk an\)[^/]*$\|.*\.wav\|.*/pict.*jpg\|plan .*\|.*/[0-9. ()]*.\.\(tif\|jpg\)\|.*/\(192.168\|7komma7\|abrechnung\|act\(os\|rapid\)\|ada \|adipositas\|advan[tz]ia\|afghanistan\|aida-studie\|äkd\|akag\|aktuell\|amd phenom\|amper\|anfrage\|angebot\|anleit\|anmeld\|antrag\|antwort\|aok \|apo-bank\|approbat\|artikel\|ärzt\|auf\(trag\|zug\)\|aus\(geschrieben\|richt\|stehen\)\|autofax\|avoid\|b2b\|bad heilbrunn\|bahn\|barmenia\|base \|basin\|bay\(\.\|eris\)\|befr\(agu\|eiu\)\|behand\|begleit\|berlin\( ak\|sulin\)\|dmp-wieder\|avp\|axa\|blahusch\|easd\|filelist\|motivat\|patmit\|predictive\|programme\|prüfung\|pumpengutachten\|patient\|patmit\|schulungs\|sidebar\|unbekannt\)[^/]*$' \( -not -iregex '.*an fax.*' -o -iregex '.*arztbrief.*' \) -name '*'|sort > "$liste"
+find $D -mindepth 1 -maxdepth 3 -newermt 20091231 -type f -not -iregex '.*\(azubi\|anforderung\|bei geräten\|dmp-daten\|diabetestag\|dokumentation.*htm\|fortbildung\|hausärzteverb\|schweigepflichts+entbindung\|vhk an\)[^/]*$\|.*\.wav\|.*/pict.*jpg\|plan .*\|.*/[0-9. ()]*.\.\(tif\|jpg\)\|.*/\(192.168\|7komma7\|abrechnung\|acots\|act\(os\|rapid\)\|ada \|adipositas\|advan[tz]ia\|afghanistan\|aida-studie\|äkd\|akag\|aktuell\|amd phenom\|amper\|anfrage\|angebot\|anleit\|anmeld\|antrag\|antwort\|aok \|apo-bank\|approbat\|artikel\|ärzt\|auf\(trag\|zug\)\|aus\(geschrieben\|richt\|stehen\)\|autofax\|avoid\|b2b\|bad heilbrunn\|bahn\|barmenia\|base \|basin\|bay\(\.\|eris\)\|befr\(agu\|eiu\)\|behand\|begleit\|berlin\( ak\|sulin\)\|dmp-wieder\|avp\|axa\|blahusch\|easd\|filelist\|motivat\|patmit\|predictive\|programme\|prüfung\|pumpengutachten\|patient\|patmit\|schulungs\|sidebar\|unbekannt\)[^/]*$' \( -not -iregex '.*an fax.*' -o -iregex '.*arztbrief.*' \) -name '*'|sort > "$liste"
 printf "\r$blau%d$reset zu untersuchende Dateien in $blau$D$reset gefunden, bearbeite sie ...\n" $(wc -l "$liste"|cut -f1 -d' ')
 while read -r file; do
     gefu=; # deshalb dürfen nachfolgend keine subshells verwendet werden
     nr=$(expr $nr + 1);            #    let nr=$nr+1 (geht nur in bash)
 #    [ $nr = 21 ]&&exit;
     [ $verb ]&&printf "$nr: $blau$file$reset\n"
-    DBBef="mysql --defaults-extra-file=~/.mysqlpwd quelle -s -e\"SELECT REPLACE(REPLACE(Pfad,'\\\\\\\\','/'),'$/TurboMed','/DATA/turbomed') FROM briefe WHERE name='$(basename "$file")' GROUP BY Pfad\""
+    DBBef="mysql --defaults-extra-file=~/.mysqlpwd quelle -s -e\"SELECT REPLACE(REPLACE(Pfad,'\\\\\\\\','/'),'$/TurboMed','/DATA/turbomed') FROM briefe WHERE name='$(basename "$file"|tr "'" "\\'")' GROUP BY Pfad\""
     TName=$(eval "$DBBef")
     if [ "$TName" ];then 
 #    mysql --defaults-extra-file=~/.mysqlpwd -s quelle -e"select pfad from briefe where pfad like '%|%'" => 0 Ergebnisse
@@ -34,7 +34,7 @@ while read -r file; do
       [ -f "$zeile" ]&&{ [ $verb ]&&printf "$reset\n";gefu=ja;}||{ [ $verb ]&&printf "$rot nicht$reset\n";}
      done;          #   done < <(echo "$TName");
     else
-     init=$(basename "$file"|cut -d' ' -f1|sed 's/[-,.;_:]*$//;s/^\(.\{3\}\).*/\1/')
+     init=$(basename "$file"|cut -d' ' -f1|sed "s/[-,.;_:']*$//;s/^\(.\{3\}\).*/\1/")
 #      stat "$file";
       sz=$(stat -c%s "$file")
       MT=$(stat -c%Y "$file")
@@ -66,7 +66,7 @@ while read -r file; do
           chmod 774 "$d"
         done;
       }
-      printf "%4b: %s\n" $fnr "$(basename "$file")" >> $AD;
+      printf "%4b: %s\n" $fnr "$(basename "$file"|tr "'" "\\'")" >> $AD;
       printf "${blau}%4b: $rot$file$reset\n" $fnr; 
       printf "     $DBBef\n";
       printf "     $TBef\n";
