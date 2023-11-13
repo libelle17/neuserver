b633aa1fbe8a25a3cba89eb1562beb144b7eb9a6
Gerald Schade <gerald.schade@gmx.de>
2023-11-13 01:36:35 +0100

    Version 0.47247

--- analdok.sh
+++ analdok.sh
@@ -43,10 +43,13 @@ fi;
 }
 
 haupttest() {
-find "$testVz" -type f -printf '%TY%Tm%Td%TH%TM%.2TS|%s|%p\0'|while IFS= read -r -d '' zeile; do 
+htnr=0;
+find "$testVz" -maxdepth 1 -type f -printf '%TY%Tm%Td%TH%TM%.2TS|%s|%p\0'|while IFS= read -r -d '' zeile; do 
+ printf "$htnr ...\r";
+ htnr=$(awk "BEGIN{print "$htnr"+1}");
  IFS='|' arr=($zeile);
  epo=$(date --date="$(echo "${arr[0]}"|sed 's/\(....\)\(..\)\(..\)\(..\)\(..\)\(..\)/\2\/\3\/\1 \4:\5:\6/')" +'%s');
- von=$(date -d @$(awk "BEGIN{print "$epo"-5*24*60*60}") +"%Y%m%d%H%M%S")
+ von=$(date -d @$(awk "BEGIN{print "$epo"-10*24*60*60}") +"%Y%m%d%H%M%S")
  bis=$(date -d @$(awk "BEGIN{print "$epo"+3*60*60+1}") +"%Y%m%d%H%M%S") # all mögliche Kombinationen von Zeitzonen
  dn=$(dirname "${arr[2]}");
  bn=$(basename "${arr[2]}");
@@ -56,10 +59,37 @@ find "$testVz" -type f -printf '%TY%Tm%Td%TH%TM%.2TS|%s|%p\0'|while IFS= read -r
  bef="SELECT CONCAT(DATE_FORMAT(laend,'%Y%m%d%H%i%S'),'|',pfad,'|',name) FROM dokfiles WHERE groe=\""${arr[1]}"\" AND laend BETWEEN $von AND $bis";
  mariadb --defaults-extra-file=~/.mysqlpwd quelle -Ne"$bef"|while read -r zeile; do
 # echo bef: "$bef";
-  IFS="|" ari=($zeile);
-  echo "";
+  IFS="|" ari=($zeile); # hiernach kamen die Einschübe mit false unten
+  if diff "$dn/$bn" "${ari[1]}/${ari[2]}" >/dev/null; then # wenn identisch
+    bef="SELECT * FROM briefe WHERE pfad=CONCAT(REPLACE(REPLACE(\""${ari[1]}"\",\"/DATA/turbomed\",\"$/TurboMed\"),\"/\",\"\\\\\"),\"\\\\\",\""${ari[2]}"\")";
+#    echo bef: $bef
+    gef=0;
+    while read -r zeile; do
+      gef=1;
+#      printf "${rot}Runde: ${blau}$ru$reset\n";
+      printf "${rot}gefunden: $dn/$bn$reset\n$zeile\n";
+      mv -n "$dn/$bn" "$dn/schonda/";
+#      printf "$dblau${arr[2]}$reset\n";
+#      printf "$lila${ari[0]}$reset\n";
+#      printf "$lila${ari[1]}$reset\n";
+#      printf "$lila${ari[2]}$reset\n";
+    done< <(mariadb --defaults-extra-file=~/.mysqlpwd quelle -Ne"$bef");
+    if [ $gef -eq 0 ]; then
+     printf "$lila$dn/$bn$reset nicht in Datenbank gefunden, verschiebe es nach niD!\n"
+     mv -n "$dn/$bn" "$dn/niD/";
+    fi;
+  fi;
+  ru=$(awk "BEGIN{print "$ru"+1}");
+ done;
+done;
+}
 
-if false; then
+# indb;
+# vortest;
+haupttest;
+echo Fertig!
+
+ if false; then
   if [ ! -f "${ari[1]}/${ari[2]}" ]; then
     bef="find \"${ari[1]}\" -size \"${arr[1]}\"c -newermt \"$(echo $von|sed 's/^\(........\)\(....\).*/\1 \2/')\" -not -newermt \"$(echo $bis|sed 's/^\(........\)\(....\).*/\1 \2/')\" -name \"${ari[2]}*\" -printf '%f\n'";
     echo bef: $bef;
@@ -67,9 +97,10 @@ if false; then
     neuname=$(eval "$bef");
     [ $neuname ]&& printf "${ari[2]} => $rot$neuname$reset\n";
   fi;
-fi;
+ fi;
 
-  diff "$dn/$bn" "${ari[1]}/${ari[2]}" >/dev/null 2>/dev/null;
+ if false; then
+  diff "$dn/$bn" "${ari[1]}/${ari[2]}" >/dev/null 2>&1;
   if [ $? -ne 0 ]; then
     echo "$dn/$bn" "${ari[1]}/${ari[2]}"
     echo bef: "$bef";
@@ -77,26 +108,4 @@ fi;
     diff "$dn/$bn" "${ari[1]}/${ari[2]}";
     echo ari2: ${ari[2]};
   fi;
-  if diff "$dn/$bn" "${ari[1]}/${ari[2]}" >/dev/null; then # wenn identisch
-    bef="select * from briefe where pfad like '%"${ari[2]}"%'";
-    bef="SELECT * FROM briefe WHERE pfad=CONCAT(REPLACE(REPLACE(\""${ari[1]}"\",\"/DATA/turbomed\",\"$/TurboMed\"),\"/\",\"\\\\\"),\"\\\\\",\""${ari[2]}"\")";
-#    echo bef: $bef
-    mariadb --defaults-extra-file=~/.mysqlpwd quelle -Ne"$bef"|while read -r zeile; do
-      printf "$rot gefunden!!!!!!!!!!!!!!!$reset\n";
-  #    printf "${rot}Runde: ${blau}$ru$reset\n";
-      printf "$rot$dn $bn$reset $zeile\n";
-      printf "$dblau${arr[2]}$reset\n";
-      printf "$lila${ari[0]}$reset\n";
-      printf "$lila${ari[1]}$reset\n";
-      printf "$lila${ari[2]}$reset\n";
-    done;
-  fi;
-  ru=$(awk "BEGIN{print "$ru"+1}");
- done;
-done;
-}
-
-indb;
-vortest;
-haupttest;
-echo Fertig!
+ fi;
