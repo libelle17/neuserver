8f67f5b976e3a24240604a8adc8ba712b586dbd3
Gerald Schade <gerald.schade@gmx.de>
2023-12-12 11:19:39 +0100

    Version 0.47247

--- verbindungen.sh
+++ verbindungen.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# für ipv6: powershell get-smbsession und close-smbsession -force -sessionid
 for A in $(ssh administrator@virtwin net sess|grep "\.2. "|cut -d" " -f1); do ssh administrator@virtwin net sess $A /delete /y; done
