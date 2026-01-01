cfc1c50c35446c8cdb8135236abab81eecfad146
Gerald Schade <gerald.schade@gmx.de>
2026-01-01 19:07:04 +0100

    Version 0.47247

--- zugriff.sh
+++ zugriff.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 chmod 777 /DATA/Patientendokumente/*
 chown sturm:praxis /DATA/Patientendokumente/Datenbanken/AccuChek/Diaries/pa*
+chown sturm:praxis -R /DATA/Patientendokumente/Fotos\ neu
