efbcf1b5919edc00fed072a67db98a6752e21772
root <root@linux0.fritz.box>
2026-05-02 09:22:41 +0200

    Version 0.47247

--- awkfdatei.sh
+++ awkfdatei.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/awk -f
-# ermittle jüngste Datei in pfad nach Muster n1*n2, die nicht kleiner als 80% der größten dort mit diesem Muster ist
+# ermittle jüngste Datei in pfad nach Muster n1*n2 anhand Datum im Dateinamen,
+# die nicht kleiner als 80% der größten dort mit diesem Muster ist
 BEGIN {
-#  pfad="/DATA/sql"
-#  n1="quelle--"
-#  n2=".sql"
-  cmd="ls "pfad"/"n1"*"n2" -S";
-  cmd|getline fname;
-  cmd="find "pfad" -path "fname" -printf '%s\n'"
-  cmd|getline groe;
-  mingroe=groe*0.8
-  ming=sprintf("%.0f",mingroe)
-  cmd="find "pfad" -maxdepth 1 -size +"ming"c -name "n1"'*'"n2" -printf \"%T@ %p\\n\"|sort -rn|cut -d' ' -f2"
-  cmd|getline ergf;
+  # Größte Datei ermitteln
+  cmd="ls " pfad "/" n1 "*" n2 " -S | head -1";
+  cmd | getline fname;
+  close(cmd);
+  cmd="stat --printf='%s' \"" fname "\"";
+  cmd | getline groe;
+  close(cmd);
+  mingroe = groe * 0.8;
+  ming = sprintf("%.0f", mingroe);
+
+  # Alle Dateien >= 80% der größten, nach Datum im Dateinamen sortieren
+  cmd="find " pfad " -maxdepth 1 -size +" ming "c -name '" n1 "*" n2 "' -printf '%f %p\\n' | sort -rn | head -1 | awk '{print $2}'";
+  cmd | getline ergf;
+  close(cmd);
   print ergf;
 }
