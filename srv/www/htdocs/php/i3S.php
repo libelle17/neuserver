8373e7d8202be45ef57fab4d509cc4290ccc02fe
Gerald Schade <gerald.schade@gmx.de>
2026-07-06 01:35:57 +0200

    Version 0.47247

--- srv/www/htdocs/php/i3S.php
+++ srv/www/htdocs/php/i3S.php
@@ -6,8 +6,6 @@
  $_SESSION['obRR']=0;
  $_SESSION['obOAU']=0;
  $_SESSION['obHbA1c']=0;
- $_SESSION['obUzu, Fqmin(6)=0/a']=0;
- $_SESSION['obHypo']=0;
  $_SESSION['obHyper']=0;
  $_SESSION['obNeuSt']=0;
  $_SESSION['obFuß']=0;
